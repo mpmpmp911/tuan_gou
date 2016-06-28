490ee7dd4e0589148afe47b3066955b64e671ab6
Cheryl <cheryl@cheryls-imac.local>
2016-06-28 10:45:19 +0800

    1111

---
+++ 团购项目/View/MainCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MainCollectionViewCell.h
+//  团购项目
+//
+//  Created by Cheryl on 6/27/16.
+//  Copyright © 2016 rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ 团购项目/View/MainCollectionViewCell.m
@@ -0,0 +1,21 @@
+//
+//  MainCollectionViewCell.m
+//  团购项目
+//
+//  Created by Cheryl on 6/27/16.
+//  Copyright © 2016 rick. All rights reserved.
+//
+
+#import "MainCollectionViewCell.h"
+
+
+
+
+@implementation MainCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
